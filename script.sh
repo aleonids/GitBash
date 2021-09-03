0cab1c556ca474b47f15725ac9ab0156fd082da2
leonid alejnikov <aleonids@tut.by>
2021-09-03 12:56:25 +0300

    Linux-terminal-GitBash-commands

---
+++ script.sh
@@ -0,0 +1,27 @@
+# Nano script.sh команда.
+#!/bin/bash – первой строкой в скрипте. Оболочка, которая запускается для выполнения скрипта.
+# Внести команды для скрипта
+# chmod +x script.sh – делает скрипт исполняемым.
+# ./script.sh – запустить скрипт
+
+#!/bin/bash
+# First line. The shell is launched to execute the script.
+
+# commands.
+cd course
+mkdir 1
+mkdir 2
+mkdir 3
+cd 1
+touch 1.txt 
+touch 2.txt
+touch 3.txt
+touch 1.json
+touch 2.json
+mkdir 01
+mkdir 02
+mkdir 03
+ls -la
+mv 1.txt 2.txt /c/QA/GitBUSH/course/script/course/2
+#ctrl+x - exit, y - save.
+
